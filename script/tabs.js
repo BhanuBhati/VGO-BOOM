be0fb8458a7665dec444d42dbb5bcdf579ff7520
BhanuBhati <42698017+BhanuBhati@users.noreply.github.com>
2018-09-01 18:38:06 +0530

    Initial commit

---
+++ script/tabs.js
@@ -0,0 +1,19 @@
+currenttab=document.getElementById('stats');
+currenttab.style.visibility="visible";
+function switchtabs(n)
+{
+    currenttab.style.visibility="hidden";
+    switch(n){
+        case 'button_chat':
+            currenttab=document.getElementById('chat');
+            break;
+        case 'button_history':
+            currenttab=document.getElementById('history');
+            break;
+        case 'button_stats':
+            currenttab=document.getElementById('stats');
+            break;
+    }
+    
+    currenttab.style.visibility="visible";
+}
